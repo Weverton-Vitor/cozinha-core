9f5d0e93bf0f0714bf9e011dee5ec0fee3c3ca45
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-02-27 08:03:51 -0300

    refactor(main): view receives controller

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import views
 
 
 def main():
-    supplier_view = views.SupplierView()
-    supplier_controller = controllers.SupplierController(supplier_view)
+    supplier_controller = controllers.SupplierController()
+    supplier_view = views.SupplierView(supplier_controller)
 
     supplier_controller.add_supplier("Ceasa")
     supplier_controller.add_supplier("Assaí Atacadista")
 
-    supplier_controller.show_suppliers()
+    supplier_view.display_suppliers()
 
 
 if __name__ == "__main__":
