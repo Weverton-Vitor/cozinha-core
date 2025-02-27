c404979707353bc0bfe56005577468e75237b427
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-02-27 08:05:00 -0300

    refactor(supplier_view): methods are now calling controller

--- views/supplier_view.py
+++ views/supplier_view.py
@@ -1,8 +1,19 @@
 import entities
-
+from controllers import supplier_controller
 
 class SupplierView:
-    def display_suppliers(self, suppliers: list[entities.Supplier]):
+    __controller: supplier_controller.SupplierController
+    
+    def __init__(self, controller: supplier_controller.SupplierController):
+        self.__controller = controller
+        pass
+
+    def create_supplier(self, name: str):
+        self.__controller.add_supplier(name)
+        self.show_message("Fornecedor adicionado com sucesso: " + name)
+        
+    def display_suppliers(self):
+        suppliers = self.__controller.get_suppliers()
         for supplier in suppliers:
             print(supplier)
 
