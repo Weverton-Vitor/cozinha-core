02ecb55a06bea4fa6439af67b29ad597b69d09f8
wevertonvitor <wevertonvitor01@gmail.com>
2025-03-05 20:36:37 -0300

    fix(valdiators): adding new polices to validade username

--- src/validators/password_validator.py
+++ src/validators/password_validator.py
@@ -3,10 +3,12 @@ import exceptions
 from validators.interface_password_validator import IPasswordValidator
 
 class PasswordValidator(IPasswordValidator):
+    min_password_length = 8
+    max_password_length = 128
     
     def validate(self, password: str) -> bool:
-        if not (8 <= len(password) <= 64):
-            raise exceptions.InvalidPasswordException("A senha deve ter entre 8 e 64 caracteres.")
+        if not (self.min_password_length <= len(password) <= self.max_password_length):
+            raise exceptions.InvalidPasswordException(f"A senha deve ter entre {self.min_password_length} e {self.max_password_length} caracteres.")
 
         if not re.search(r'[A-Z]', password):
             raise exceptions.InvalidPasswordException("A senha deve conter pelo menos uma letra maiúscula.")
