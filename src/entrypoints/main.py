5c73c09c7ddd99dcd16921d8456bf9d5e9b1d961
wevertonvitor <wevertonvitor01@gmail.com>
2025-03-05 20:33:24 -0300

    fix(valdiators): adding new polices to validade username

--- src/entrypoints/main.py
+++ src/entrypoints/main.py
@@ -6,12 +6,16 @@ def main():
     supplier_controller = controllers.SupplierController()
     supplier_view = views.SupplierView(supplier_controller)
 
+    supplier_controller.add_supplier("", "Gf3/@sfsdeasaFsd")
+    supplier_controller.add_supplier("Assaí Atacadista", "Gf3/@sfsdeasaFsd")
+    supplier_controller.add_supplier("Assái2", "testeeeeeA2@")
+    supplier_controller.add_supplier("Assaí Ataca", "1234")
+    supplier_controller.add_supplier("Assaí Ataca", "testeeeee")
+    supplier_controller.add_supplier("Assaí Ataca", "testeeeeeA")
+    supplier_controller.add_supplier("Assaí Ataca", "testeeeeeA2")
+    supplier_controller.add_supplier("Assaí Ataca", "testeeeeeA2@")
+
     supplier_controller.add_supplier("Ceasa", "Gf3/@sfsdeasaFsd")
-    supplier_controller.add_supplier("Assaí Atacadista", "1234")
-    supplier_controller.add_supplier("Assaí Atacadista", "testeeeee")
-    supplier_controller.add_supplier("Assaí Atacadista", "testeeeeeA")
-    supplier_controller.add_supplier("Assaí Atacadista", "testeeeeeA2")
-    supplier_controller.add_supplier("Assaí Atacadista", "testeeeeeA2@")
 
 
     supplier_view.display_suppliers()
--- src/validators/username_validator.py
+++ src/validators/username_validator.py
@@ -2,7 +2,13 @@ import exceptions
 from validators.interface_username_validator import IUsernameValidator
 
 class UsernameValidator(IUsernameValidator):
+    max_username_length = 12
     def validate(self, username: str) -> bool:
         if not username.strip():
             raise exceptions.InvalidUsernameException("O nome de usuário não pode estar vazio.")
+        if len(username) > self.max_username_length:
+            raise exceptions.InvalidUsernameException(f"O nome de usuário não pode ter mais que {self.max_username_length} caracteres.")
+
+        if any(char.isdigit() for char in username):
+            raise exceptions.InvalidUsernameException("O nome de usuário não pode conter números.")
         return True
