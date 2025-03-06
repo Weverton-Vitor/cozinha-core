103eb87a823b067446fb9d38c4471c81a7f8fb8a
Gisele Silva <gisellegomezz36@gmail.com>
2025-03-06 01:55:35 -0300

    update: add sqlite repository instance

--- src/entrypoints/main.py
+++ src/entrypoints/main.py
@@ -1,16 +1,17 @@
 import controllers
 import views
-
+from repositories import SQLiteRepository
 
 def main():
-    supplier_controller = controllers.SupplierController()
+    supplier_controller = controllers.SupplierController(repository=SQLiteRepository())
     supplier_view = views.SupplierView(supplier_controller)
 
     supplier_view.create_supplier("Ceasa", "Gf3/@sfsdeasaFsd")
-    supplier_view.create_supplier("Assaí Atacadista", "1234")
-    supplier_view.create_supplier("Assaí Atacadista", "testeeeee")
-    supplier_view.create_supplier("Assaí Atacadista", "testeeeeeA")
-    supplier_view.create_supplier("Assaí Atacadista", "testeeeeeA2")
+    supplier_view.create_supplier("Assaí", "12348")
+    supplier_view.create_supplier("Assaí_um", "testeeeee")
+    supplier_view.create_supplier("Assaí_dois", "testeeeeeA")
+    supplier_view.create_supplier("Assaí_tres", "testeeeeeA2")
+    supplier_view.create_supplier("Assaí_quatro", "testeeeeeA2@")
     supplier_view.create_supplier("Assaí Atacadista", "testeeeeeA2@")
 
     supplier_view.display_suppliers()
