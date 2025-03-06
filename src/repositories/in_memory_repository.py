692595c51c90c23cdaa935d76d10ee9672330567
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-03-05 21:56:19 -0300

    fix: addition of delete, update and get methods

--- src/repositories/in_memory_repository.py
+++ src/repositories/in_memory_repository.py
@@ -9,6 +9,21 @@ class InMemoryRepository(ISuppliersRepository):
     
     def create(self, supplier: entities.Supplier) -> None:
         self.__suppliers.append(supplier)
+
+    def delete(self, username: str) -> None:
+        supplier_to_delete = next((sup for sup in self.__suppliers if sup.username == username), None)
+        if supplier_to_delete:
+            self.__suppliers.remove(supplier_to_delete)
+
+    def update(self, username: str, supplier: entities.Supplier) -> None:
+        for index, sup in enumerate(self.__suppliers):
+            if sup.username == username:
+                self.__suppliers[index] = supplier
+                return
+
+    def get(self, username: str) -> entities.Supplier | None:
+        supplier = next((sup for sup in self.__suppliers if sup.username == username), None)
+        return supplier
     
     def getAll(self) -> list[entities.Supplier]:
         return self.__suppliers
