2b6ac829ded70525468fe42e3690625651f407b5
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-03-05 22:01:11 -0300

    fix: repository option and creation with supplier view

--- src/entrypoints/main.py
+++ src/entrypoints/main.py
@@ -1,24 +1,19 @@
 import controllers
 import views
 
-
 def main():
     supplier_controller = controllers.SupplierController()
     supplier_view = views.SupplierView(supplier_controller)
 
-    supplier_controller.add_supplier("", "Gf3/@sfsdeasaFsd")
-    supplier_controller.add_supplier("Assaí Atacadista", "Gf3/@sfsdeasaFsd")
-    supplier_controller.add_supplier("Assái2", "testeeeeeA2@")
-    supplier_controller.add_supplier("Assaí Ataca", "1234")
-    supplier_controller.add_supplier("Assaí Ataca", "testeeeee")
-    supplier_controller.add_supplier("Assaí Ataca", "testeeeeeA")
-    supplier_controller.add_supplier("Assaí Ataca", "testeeeeeA2")
-    supplier_controller.add_supplier("Assaí Ataca", "testeeeeeA2@")
-
-    supplier_controller.add_supplier("Ceasa", "Gf3/@sfsdeasaFsd")
+    supplier_view.create_supplier("Ceasa", "Gf3/@sfsdeasaFsd", 1)
+    supplier_view.create_supplier("Assaí Atacadista", "1234", 1)
+    supplier_view.create_supplier("Assaí Atacadista", "testeeeee", 1)
+    supplier_view.create_supplier("Assaí Atacadista", "testeeeeeA", 1)
+    supplier_view.create_supplier("Assaí Atacadista", "testeeeeeA2", 1)
+    supplier_view.create_supplier("Assaí Atacadista", "testeeeeeA2@", 1)
 
 
-    supplier_view.display_suppliers()
+    supplier_view.display_suppliers(1)
 
 if __name__ == "__main__":
     main()
