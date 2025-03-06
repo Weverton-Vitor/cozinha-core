be9acbed05a2778ac41bc1b08656074b989e5817
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-03-05 21:57:54 -0300

    Addition of an in-memory repository instance

--- src/controllers/supplier_controller.py
+++ src/controllers/supplier_controller.py
@@ -1,15 +1,17 @@
 import entities
+import repositories.in_memory_repository
 import services
 import exceptions
 import validators
+import repositories
 
 
 class SupplierController:
-    __suppliers: list[entities.Supplier]
     __service: services.SuppliersService
+    __in_memory_repository: repositories.in_memory_repository.InMemoryRepository
 
-    def __init__(self, service: services.SuppliersService=None):
-        self.__suppliers = []
+    def __init__(self, service: services.SuppliersService=None, 
+                 in_memory_repository: repositories.ISuppliersRepository=None):
 
         if service is None:
             self.__service = services.SuppliersService(
@@ -19,16 +21,24 @@ class SupplierController:
         else:
             self.__service = service
 
-    def add_supplier(self, name: str, password: str):
+        if in_memory_repository is None:
+            self.__in_memory_repository = repositories.in_memory_repository.InMemoryRepository()
+        else:
+            self.__in_memory_repository = in_memory_repository
+
+
+    def add_supplier(self, name: str, password: str, option: bool):
         try:
             if self.__service.validate_credentials(name, password):
                 sup = entities.Supplier(name, password)
-                self.__suppliers.append(sup)
+                if option == 1:
+                    self.__in_memory_repository.create(sup)
 
         except exceptions.InvalidUsernameException as e:
             print(f"Erro de nome de usuário: {e}")
         except exceptions.InvalidPasswordException as e:
             print(f"Erro de senha: {e}")
 
-    def get_suppliers(self):
-        return self.__suppliers
+    def get_suppliers(self, option):
+        if option == 1:
+            return self.__in_memory_repository.getAll()
