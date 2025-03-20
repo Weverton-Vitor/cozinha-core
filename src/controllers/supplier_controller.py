357a5b9ac9ca258e9ef29c24a4175394bac53b17
Weverton <wevertonvitor01@gmail.com>
2025-03-20 08:17:08 -0300

    feat(controller): Adding CRUD methods

--- src/controllers/supplier_controller.py
+++ src/controllers/supplier_controller.py
@@ -1,4 +1,6 @@
 import entities
+import exceptions.DeleteException
+import exceptions.LookupException
 import services
 import exceptions
 import repositories
@@ -35,6 +37,24 @@ class SupplierController:
         except exceptions.InvalidPasswordException as e:
             print(f"Erro de senha: {e}")
         except exceptions.PersistenceException as e:
+            print(f"Erro de persistência: {e}")
+            
+    def update_supplier(self, name: str, supplier: entities.Supplier):
+        try:
+            self.__repository.update(name=name, supplier=supplier)
+        except exceptions.PersistenceException as e:
+            print(f"Erro de persistência: {e}")
+            
+    def get_supplier(self, name: str) -> entities.Supplier:
+        try:
+            return self.__repository.get(name=name)
+        except exceptions.LookupException as e:
+            print(f"Erro de persistência: {e}")
+    
+    def delete_supplier(self, name: str) -> entities.Supplier:
+        try:
+            return self.__repository.delete(name=name)
+        except exceptions.DeleteException as e:
             print(f"Erro de persistência: {e}")
 
     def get_suppliers(self):
