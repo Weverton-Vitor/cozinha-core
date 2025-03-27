7fb457c91c97de323d3ebbb13ea928d5062b958f
wevertonvitor <wevertonvitor01@gmail.com>
2025-03-27 07:57:28 -0300

    Refact(singleton): remove use case example

--- src/decorators/singleton.py
+++ src/decorators/singleton.py
@@ -9,22 +9,3 @@ def singleton(cls):
         return instances[cls]
 
     return wrapper
-
-
-@singleton
-class SettingsSingleton:
-    def __init__(self, some_value) -> None:
-        self.some_value = some_value
-        self.debug = True
-
-
-a = SettingsSingleton()
-a.some_value = 10
-print("a.some_value:", a.some_value)
-
-b = SettingsSingleton()
-b.debug = False
-print("b.some_value:", b.some_value)
-
-print(SettingsSingleton().some_value)
-print(SettingsSingleton().debug)
