fc6bbc62711c6a3b2ae8fc7c2ce32c92c165919f
wevertonvitor <wevertonvitor01@gmail.com>
2025-03-27 07:52:52 -0300

    Style(Repository): PEP8 formatting

--- src/repositories/kitchens/kitchen_sqlite_repository.py
+++ src/repositories/kitchens/kitchen_sqlite_repository.py
@@ -1,11 +1,17 @@
 from repositories import SQLiteRepository
 from entities import Kitchen
 
+
 class SQLiteKitchenRepository(SQLiteRepository[Kitchen]):
-    def __init__(self, db_path: str = 'data/kitchens.db'):
+    def __init__(self, db_path: str = "data/kitchens.db"):
         fields = [
-            "name TEXT PRIMARY KEY", "user_name TEXT", "password TEXT",
-            "address TEXT", "phone_number TEXT", "email TEXT",
-            "created_at TEXT", "status INTEGER"
+            "name TEXT PRIMARY KEY",
+            "user_name TEXT",
+            "password TEXT",
+            "address TEXT",
+            "phone_number TEXT",
+            "email TEXT",
+            "created_at TEXT",
+            "status INTEGER",
         ]
         super().__init__(db_path, "kitchens", Kitchen, fields)
