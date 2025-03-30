f1eab9321fca368b2045601bb052699826f538f2
Weverton Vitor <wevertonvitor01@gmail.com>
2025-03-30 20:35:11 -0300

    feat(infra/dao): Implementing new methods from interfaces

--- src/infra/dao/array_kitchen_dao.py
+++ src/infra/dao/array_kitchen_dao.py
@@ -1,18 +1,51 @@
-from typing import TypeVar, List
+from typing import List
 from dao import interfaces
-
-T = TypeVar("T")
+from business import entities
 
 
 class ArrayKitchenDAO(interfaces.IKitchenDAO):
     def __init__(self):
-        self._items: List[T] = []
+        self._items: List[entities.Kitchen] = []
 
     def connect(self):
-        pass  # Nenhuma conexão necessária para armazenamento em memória
+        pass
 
     def query(self, condition=lambda item: True):
-        return [item for item in self._items if condition(item)]
+        raise NotImplementedError()
+
+    def create(self, item: entities.Kitchen) -> None:
+        self._items.append(item)
+
+    def delete(self, identifier: str) -> None:
+        item_to_delete = next(
+            (
+                item
+                for item in self._items
+                if getattr(item, "username", None) == identifier
+            ),
+            None,
+        )
+        if item_to_delete:
+            self._items.remove(item_to_delete)
+
+    def update(self, identifier: str, item: entities.Kitchen) -> None:
+        for index, existing_item in enumerate(self._items):
+            if getattr(existing_item, "username", None) == identifier:
+                self._items[index] = item
+                return
+
+    def get(self, identifier: str) -> entities.Kitchen | None:
+        return next(
+            (
+                item
+                for item in self._items
+                if getattr(item, "username", None) == identifier
+            ),
+            None,
+        )
+
+    def getAll(self) -> List[entities.Kitchen]:
+        return self._items
 
     def close(self):
-        pass  # Nenhuma conexão para fechar
+        pass
--- src/infra/dao/array_supplier_dao.py
+++ src/infra/dao/array_supplier_dao.py
@@ -1,18 +1,37 @@
-from typing import TypeVar, List
+from typing import List
+from business import entities
 from dao import interfaces
-
-T =  TypeVar("T")
 
 
 class ArraySupplierDAO(interfaces.ISupplierDAO):
     def __init__(self):
-        self._items: List[T] = []
+        self._items: List[entities.Supplier] = []
 
     def connect(self):
         pass  # Nenhuma conexão necessária para armazenamento em memória
 
     def query(self, condition=lambda item: True):
-        return [item for item in self._items if condition(item)]
+        raise NotImplementedError()
+    
+    def create(self, item: entities.Supplier) -> None:
+        self._items.append(item)
+
+    def delete(self, identifier: str) -> None:
+        item_to_delete = next((item for item in self._items if getattr(item, "username", None) == identifier), None)
+        if item_to_delete:
+            self._items.remove(item_to_delete)
+
+    def update(self, identifier: str, item: entities.Supplier) -> None:
+        for index, existing_item in enumerate(self._items):
+            if getattr(existing_item, "username", None) == identifier:
+                self._items[index] = item
+                return
+
+    def get(self, identifier: str) -> entities.Supplier | None:
+        return next((item for item in self._items if getattr(item, "username", None) == identifier), None)
+
+    def getAll(self) -> List[entities.Supplier]:
+        return self._items
 
     def close(self):
         pass  # Nenhuma conexão para fechar
