fc4764f8ec9bf29cb2f40fe4bbc30783eed4dec3
Weverton Vitor <wevertonvitor01@gmail.com>
2025-04-02 21:23:48 -0300

    feat(infra/factory): Adding dao factory interface

---
+++ src/infra/dao/__init__.py
@@ -0,0 +1,13 @@
+from array_kitchen_dao import ArrayKitchenDAO
+from array_supplier_dao import ArraySupplierDAO
+from sqlite_kitchen_dao import SQLiteKitchenDAO
+from sqlite_supplier_dao import SQLiteRepository
+import interfaces
+
+__all__ = [
+    ArrayKitchenDAO,
+    ArraySupplierDAO,
+    SQLiteKitchenDAO,
+    SQLiteRepository,
+    interfaces,
+]
--- src/infra/factories/interfaces/i_dao_factory.py
+++ src/infra/factories/interfaces/i_dao_factory.py
@@ -1,1 +1,14 @@
 # TODO write IDAOFactory interface code
+
+from abc import ABC, abstractmethod
+from infra import dao
+
+
+class IDAOFactory(ABC):
+    @abstractmethod
+    def get_supplier_repository(self) -> dao.interfaces.ISupplierDAO:
+        pass
+
+    @abstractmethod
+    def get_kitchen_repository(self) -> dao.interfaces.IKitchenDAO:
+        pass
