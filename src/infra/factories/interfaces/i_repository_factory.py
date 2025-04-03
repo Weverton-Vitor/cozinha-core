7b9786b831cbaf8babd4fdfff62851f17c770572
Weverton Vitor <wevertonvitor01@gmail.com>
2025-04-02 21:18:59 -0300

    cleanup(infra/factory): remove comments

--- src/infra/factories/interfaces/i_repository_factory.py
+++ src/infra/factories/interfaces/i_repository_factory.py
@@ -1,9 +1,7 @@
-# Definição das interfaces dos repositórios
 from abc import ABC, abstractmethod
 from infra import repositories
 
 
-# Definição da interface da fábrica
 class IRepositoryFactory(ABC):
     @abstractmethod
     def get_supplier_repository(self) -> repositories.interfaces.ISupplierRepository:
