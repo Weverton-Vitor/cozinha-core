abbcdcc5c14d72f1982c01e0c958bc8e1601ba87
mariasgla <mariasagurgel@gmail.com>
2025-04-03 02:36:29 -0300

    Implementação do InMemorySuppliersRepository conforme a interface ISupplierRepository

--- src/infra/repositories/in_memory_supplier_repository.py
+++ src/infra/repositories/in_memory_supplier_repository.py
@@ -1,6 +1,29 @@
-from infra import repositories
+from infra.repositories import interfaces
+from business.entities import Supplier
 
-# TODO write InMemorySuppliersRepository class that implements ISupplierRepository interface
+class InMemorySuppliersRepository(interfaces.ISupplierRepository):
+    def __init__(self):
 
-class InMemorySuppliersRepository(repositories.interfaces.ISupplierRepository):
-    pass
+        self._suppliers = {}
+
+    def create(self, supplier: Supplier) -> None:
+        
+        self._suppliers[supplier.username] = supplier
+
+    def delete(self, username: str) -> None:
+        
+        if username in self._suppliers:
+            del self._suppliers[username]
+
+    def update(self, username: str, supplier: Supplier) -> None:
+        
+        if username in self._suppliers:
+            self._suppliers[username] = supplier
+
+    def get(self, username: str) -> Supplier | None:
+        
+        return self._suppliers.get(username)
+
+    def getAll(self) -> list[Supplier]:
+        
+        return list(self._suppliers.values())
