be9a7cc4aefcc92722a103818cf93d98f7957ebc
Weverton Vitor <wevertonvitor01@gmail.com>
2025-04-02 21:09:59 -0300

    feat(infra/factory): Adding repository factory interface

--- src/infra/factories/interfaces/i_repository_factory.py
+++ src/infra/factories/interfaces/i_repository_factory.py
@@ -1,1 +1,14 @@
-# TODO write IRepositoryFactory interface code
+# Definição das interfaces dos repositórios
+from abc import ABC, abstractmethod
+from infra import repositories
+
+
+# Definição da interface da fábrica
+class IRepositoryFactory(ABC):
+    @abstractmethod
+    def get_supplier_repository(self) -> repositories.interfaces.ISupplierRepository:
+        pass
+
+    @abstractmethod
+    def get_kitchen_repository(self) -> repositories.interfaces.IKitchenRepository:
+        pass
