eac3c3b56e6bffdb6493ed5d632209594bc71fc8
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-04-27 18:48:26 -0300

    Update sql_repository_factory.py

--- src/infra/factories/sql_repository_factory.py
+++ src/infra/factories/sql_repository_factory.py
@@ -6,6 +6,9 @@ import factories
 class SQLiteRepositoryFactory(factories.interfaces.IRepositoryFactory):
     def get_supplier_repository(self) -> repositories.interfaces.ISupplierRepository:
         return repositories.SQLiteSupplierRepository()
-
-    def get_kitchen_repository(self) -> repositories.interfaces.IKitchenRepository:
+    
+def get_kitchen_repository(self) -> repositories.interfaces.IKitchenRepository:
         return repositories.SQLiteKitchenRepository()
+
+def get_order_repository(self) -> repositories.interfaces.IOrderRepository:
+        return repositories.SQLiteOrderRepository()
