db6e1bfbc2d25b40eaeb8fe7478b2b9aec6fc3fc
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-04-26 21:44:09 -0300

    Add files via upload

---
+++ src/business/entities/order.py
@@ -0,0 +1,34 @@
+from typing import List
+import Products
+from business.memento import order_memento
+
+class Order:
+    __id: str
+    __products: List[Products]
+
+    def __init__(self, id: str, products: List[Products]):
+        self.__id = id
+        self.__products = products
+
+    def set_id(self, id: str):
+        self.__id = id
+
+    def get_id(self):
+        return self.__id
+
+    def set_products(self, products: List[Products]):
+        self.__products = products
+
+    def get_products(self):
+        return self.__products
+    
+    def save_state(self):
+        return order_memento.OrderMemento(self.__id, self.__products)
+    
+    def restore_state(self, memento: order_memento.OrderMemento):
+        self.__id = memento.get_id()
+        self.__products = memento.get_products()
+
+    def __str__(self):
+        return f"Order {{ Id: {self.__id}}}"
+
