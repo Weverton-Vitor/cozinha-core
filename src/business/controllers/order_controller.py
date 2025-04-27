ed7b1e822596a7477c6e23bc8f7ddd30c3801389
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-04-27 18:53:58 -0300

    Add files via upload

---
+++ src/business/controllers/order_controller.py
@@ -0,0 +1,28 @@
+from business import entities
+from infra import repositories
+from business import commands
+
+
+class OrderController:
+    __repository: repositories.IOrderRepository
+    __command: commands.Command
+
+    def __init__(
+        self,
+        repository: repositories.IOrderRepository = None,
+        command: commands.Command = None
+    ):
+        if repository is None:
+            self.__repository = repositories.InMemoryOrderRepository()
+        else:
+            self.__repository = repository
+        self.__command = command
+
+def set_command(self, command: commands.Command):
+    self.__command = command
+
+def execute_command(self):
+    self.__command.execute()
+
+def create_order():
+    return 
