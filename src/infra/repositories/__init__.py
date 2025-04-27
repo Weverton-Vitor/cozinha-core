367a6d5549f59f4e28e01b9e8cf0e02aba47a29e
Gisele Silva <gisellegomezz36@gmail.com>
2025-04-26 23:05:35 -0300

    feat(infra/repository): Add Product inits

--- src/infra/repositories/__init__.py
+++ src/infra/repositories/__init__.py
@@ -1,13 +1,17 @@
-from infra.repositories.interfaces import IKitchenRepository, ISupplierRepository
+from infra.repositories.interfaces import IKitchenRepository, ISupplierRepository, IProductRepository
 from infra.repositories.in_memory_kitchen_repository import InMemoryKitchenRepository
 from infra.repositories.in_memory_supplier_repository import InMemorySuppliersRepository
+from infra.repositories.in_memory_product_repository import InMemoryProductRepository
 from infra.repositories.sqlite_kitchen_repository import SQLiteKitchenRepository
 from infra.repositories.sqlite_supplier_repository import SQLiteSupplierRepository
+from infra.repositories.sqlite_product_repository import SQLiteProductRepository
 
 __all__ = [
-    IKitchenRepository, ISupplierRepository,
+    IKitchenRepository, ISupplierRepository, IProductRepository,
     InMemoryKitchenRepository,
     InMemorySuppliersRepository,
+    InMemoryProductRepository,
     SQLiteKitchenRepository,
-    SQLiteSupplierRepository
+    SQLiteSupplierRepository,
+    SQLiteProductRepository
 ]
