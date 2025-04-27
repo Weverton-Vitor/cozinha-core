8717112c96f9f056db4f26dc03c84b28c8f49bc7
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-04-27 18:50:54 -0300

    Update command.py

--- src/business/commands/command.py
+++ src/business/commands/command.py
@@ -2,6 +2,10 @@ from abc import ABC, abstractmethod
 
 class Command(ABC):
     @abstractmethod
+    def __init__(id: str):
+        pass
+
+    @abstractmethod
     def execute(self):
         pass
 
