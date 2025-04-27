d413ef950876002de82e8de606e82b025c8c6dcf
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-04-27 18:48:08 -0300

    Update in_memory_repository_factory.py

--- src/infra/factories/in_memory_repository_factory.py
+++ src/infra/factories/in_memory_repository_factory.py
@@ -9,3 +9,6 @@ class InMemoryRepositoryFactory(factories.interfaces.IRepositoryFactory):
 
     def get_kitchen_repository(self) -> repositories.interfaces.IKitchenRepository:
         return repositories.InMemoryKitchenRepository()
+    
+    def get_order_repository(self) -> repositories.interfaces.IOrderRepository:
+        return repositories.InMemoryOrderRepository()
