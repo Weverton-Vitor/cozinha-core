4019d79313f46cd5c47d36c7cd0305e820600fbc
Gisele Silva <gisellegomezz36@gmail.com>
2025-04-26 23:02:45 -0300

    feat(infra/repository): Add SQLite Product repository

---
+++ src/infra/repositories/sqlite_product_repository.py
@@ -0,0 +1,24 @@
+from infra import repositories
+from business import entities
+from factories.sqlite_dao_factory import SQLIteDAOFactory
+
+
+class SQLiteProductRepository(repositories.interfaces.IProductRepository):
+    def __init__(self):
+        dao_factory = SQLIteDAOFactory()
+        self.product_dao = dao_factory.get_product_dao()
+
+    def create(self, product: entities.Product) -> None:
+        self.product_dao.create(product)
+
+    def delete(self, product_id: str) -> None:
+        self.product_dao.delete(product_id)
+
+    def update(self, product_id: str, product: entities.Product) -> None:
+        self.product_dao.update(product_id, product)
+
+    def get(self, product_id: str) -> entities.Product | None:
+        return self.product_dao.get(product_id)
+
+    def getAll(self) -> list[entities.Product]:
+        return self.product_dao.getAll()
