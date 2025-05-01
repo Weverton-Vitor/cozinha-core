8b69e74fc313739d191a18a1b61f6cf35592ba76
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2025-05-01 19:38:34 -0300

    Update get_order_command.py

--- src/business/commands/get_order_command.py
+++ src/business/commands/get_order_command.py
@@ -1,11 +1,10 @@
 from business.commands import Command
+from infra import repositories
 
 class GetOrderCommand(Command):
     def __init__(id: str):
         return 
     
-    def execute():
-        return
-    
-    def undo():
-        return
+    def execute(self, repository: repositories.IOrderRepository):
+        return self.__repository.get(identifier=self.__id)
+
