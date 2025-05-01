23584e2696b6c3c8620277148ef6674a6d26b61b
Gisele Silva <gisellegomezz36@gmail.com>
2025-05-01 12:16:31 -0300

    Update DAO's inits

--- src/infra/dao/interfaces/__init__.py
+++ src/infra/dao/interfaces/__init__.py
@@ -1,5 +1,6 @@
 from .kitchen_dao import IKitchenDAO
 from .supplier_dao import ISupplierDAO
 from .product_dao import IProductDAO
+from .order_dao import IOrderDAO
 
-__all__ = [IKitchenDAO, ISupplierDAO, IProductDAO]
+__all__ = [IKitchenDAO, ISupplierDAO, IProductDAO, IOrderDAO]
